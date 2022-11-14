64c01b5ce088a018f986466fd170afff83421291
Admin <anstdor@gmail.com>
2022-11-14 20:05:05 +0300

    Hash part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+            public static void main(String[] args) {
+
+                var productList = new ProductList();
+
+                productList.add("Сельдерей", 50f,10f);
+                productList.add("Укроп", 100f, 25f);
+                productList.add("Тунец", 150f, 13f);
+                productList.add("Манго", 200f, 31f);
+                productList.add("Чипсы", 250f, 14f);
+                productList.add("Кабачок", 125f, 35f);
+                productList.add("Морковка", 50f,10f);
+                productList.add("Салат",330f,55f);
+
+                productList.show();
     }
 }
